0505b6454fbef1747161b7452ca3cc7219b1c07c
vicente <vnagatoshim@gmail.com>
2014-03-20 23:46:33 -0400

    cambios de prueba78

--- db/migrate/20131012112207_change_columns_to_customer.rb
+++ db/migrate/20131012112207_change_columns_to_customer.rb
@@ -1,7 +1,12 @@
 class ChangeColumnsToCustomer < ActiveRecord::Migration
-  def change
-    change_column :customers, :city_id, :integer
+  def up
+    remove_column :customers, :city_id
+    add_column :customers, :city_id, :integer, null: false
     remove_column :customers, :department
   end
-
+  def down
+    remove_column :customers, :city_id
+    add_column :customers, :city_id, :string
+    add_column :customers, :department, :string
+  end
 end
